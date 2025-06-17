1794a9af08f1e3702d153902cbbb809bcbe869bf
degleg <legoff.d@outlook.com>
2025-06-17 16:02:51 +0200

    Add Timer js comments building steps / brainstorming

---
+++ javascript/timer/timer.js
@@ -0,0 +1,40 @@
+// Get the create timer div
+// Put an event listener on the create button
+// Get the timer container div
+// On create button click, create a timer in the container div
+// When done, create button is greyed out
+
+
+// Timer is made up of:
+// a div that contains
+// a button for play/pause
+// a stop/reset button, inactive on creation
+// numbers for countdown
+
+// Each button needs an event listener
+
+// the play button turns to pause when started, back to play when paused, then to done when timer runs out (text content)
+// the play button starts and pauses the timer
+// the play button activates the reset button
+// when timer runs out, the play button (then done button) is inactive
+
+// the reset button turns play button back to play, countdown back to initial value, and self to inactive
+
+// Timer logic
+// Start with hardcoded time (5 minutes = 300s = 300.000ms)
+// Turns red when reaches 0
+
+//Functions needed: createTimer, launchTimer (playTimer?), pauseTimer, resumeTimer(?), resetTimer
+
+
+
+// Added complexity for later:
+// Let user set length
+// Multiple timers (need some form of ids so controls work properly/target only their own timer)
+// Reverse timer (count up)
+
+
+
+
+
+
